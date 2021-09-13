6bc67e7003c28850ebfb9b11f7aab34c78b89aab
Tedtoev Azamat <ajam88@mail.ru>
2021-09-13 16:02:28 +0300

    1

---
+++ M07/BubbleSort.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace M07
+{
+    public class BubbleSort
+    {
+        public IBubbleSortStrategy SortStrategy { get; private set; }
+        public TypeOfComparison ComparisonType { get; private set; }
+        public TypeOfOrder OrderType { get; private set; }
+        public int[,] Matrix { get; private set; }
+
+        public BubbleSort(int[,] matrix, IBubbleSortStrategy sortStrategy)
+        {
+            if (matrix == null)
+            {
+                throw new ArgumentNullException(nameof(matrix));
+            }
+
+            Matrix = matrix;
+            ComparisonType = sortStrategy.ComparisonType;
+            OrderType = sortStrategy.OrderType;
+
+        }
+
+        public int[,] BubbleSortByRowsSum()
+        {
+            var rowSummArray = new int[Matrix.GetLength(0)];
+            var sum = 0;
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < Matrix.GetLength(1); j++)
+                {
+                    sum += Matrix[i, j];
+                }
+
+                rowSummArray[i] = sum;
+                sum = 0;
+            }
+
+            return RowsBubbleSort(rowSummArray);
+        }
+
+        public int[,] BubbleSortByRowsExtremum()
+        {
+            var extremums = new int[Matrix.GetLength(0)];
+
+            for (int i = 0; i < Matrix.GetLength(0); i++)
+            {
+                var extremum = Matrix[i, 0];
+
+                for (int j = 0; j < Matrix.GetLength(1) - 1; j++)
+                {
+                    if (OrderType == TypeOfOrder.Maximum)
+                    {
+                        if (extremum < Matrix[i, j + 1])
+                        {
+                            extremum = Matrix[i, j + 1];
+                        }
+                    }
+                    else
+                    {
+                        if (extremum > Matrix[i, j + 1])
+                        {
+                            extremum = Matrix[i, j + 1];
+                        }
+                    }
+                }
+
+                extremums[i] = extremum;
+            }
+
+            return RowsBubbleSort(extremums);
+        }
+
+        private int[,] RowsBubbleSort(int[] rows)
+        {
+            if (Matrix == null || rows == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            for (int i = 0; i < rows.Length - 1; i++)
+            {
+                for (int j = 1; j < rows.Length; j++)
+                {
+                    if ((ComparisonType == TypeOfComparison.Ascending && rows[i] > rows[j]) ||
+                        (ComparisonType == TypeOfComparison.Descending && rows[i] < rows[j]))
+                    {
+                        SwapRows(i, j);
+                    }
+                }
+            }
+
+            return Matrix;
+        }
+
+        private void SwapRows(int firstString, int secondString)
+        {
+            for (int j = 0; j < Matrix.GetLength(1); j++)
+            {
+                var a = Matrix[firstString, j];
+                Matrix[firstString, j] = Matrix[secondString, j];
+                Matrix[secondString, j] = a;
+            }
+        }
+
+        public enum TypeOfComparison
+        {
+            Ascending,
+            Descending
+        }
+
+        public enum TypeOfOrder
+        {
+            Maximum,
+            Minimum,
+            Sum
+        }
+    }
+}
---
+++ M07/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace M07
+{
+    delegate int[,] MyDelegate(int[,] twoDimensional, BubbleSort.TypeOfComparison comparisonType, BubbleSort.TypeOfOrder orderType);
+
+    public interface IBubbleSortStrategy
+    {
+        public BubbleSort.TypeOfComparison ComparisonType { get; set; }
+        public BubbleSort.TypeOfOrder OrderType { get; set; }
+    }
+    public class TestExtremumSort : IBubbleSortStrategy
+    {
+        public BubbleSort.TypeOfComparison ComparisonType { get; set; }
+        public BubbleSort.TypeOfOrder OrderType { get; set; }
+
+        public TestExtremumSort()
+        {
+            ComparisonType = BubbleSort.TypeOfComparison.Ascending;
+            OrderType = BubbleSort.TypeOfOrder.Maximum;
+        }
+    }
+    public class TestSumSort : IBubbleSortStrategy
+    {
+        public BubbleSort.TypeOfComparison ComparisonType { get; set; }
+        public BubbleSort.TypeOfOrder OrderType { get; set; }
+
+        public TestSumSort()
+        {
+            ComparisonType = BubbleSort.TypeOfComparison.Descending;
+            OrderType = BubbleSort.TypeOfOrder.Sum;
+        }
+    }
+
+    class Program
+    {
+        private static Random random = new Random();
+
+        static void Main()
+        {
+            var lenght = random.Next(3, 3);
+            var height = random.Next(3, 3);
+            var matrix = new int[lenght, height];
+            FillDemoMatrix(matrix);
+            Console.WriteLine("DemoMatrix:");
+            PrintMatrix(matrix);
+
+
+
+            Console.WriteLine("Result of Bubble Sorting in Ascending order by Sums of matrix elements:");
+
+            var testExtremumSort = new TestExtremumSort();
+            var newSort = new BubbleSort(matrix, testExtremumSort);
+            var newMatrix =  newSort.BubbleSortByRowsExtremum();
+            PrintMatrix(newMatrix);
+        }
+
+        public static void FillDemoMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(1, 20);
+                }
+            }
+        }
+
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
